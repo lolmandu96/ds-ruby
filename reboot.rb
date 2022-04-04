df747c37be70240ad10dea75c304f1f53bdbcc23
lolmandu96 <98780788+lolmandu96@users.noreply.github.com>
2022-04-04 13:29:49 +0200

    Update reboot.rb

--- reboot.rb
+++ reboot.rb
@@ -2,4 +2,4 @@ require "coloize"
 
 system("shutdown /r")
 
-out = "[".red.bold + "~".red.bold.blink + "] SHUTDOWNING CLIENT!".red.bold
+out = "[".red.bold + "~".red.bold.blink + "] REBOOTING CLIENT!".red.bold
